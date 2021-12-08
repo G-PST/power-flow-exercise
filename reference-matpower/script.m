81f096fe801e3bcbbba771e5abca7910b74a182d
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-08 09:21:04 -0700

    feat: Update matlab script.m ✨

--- reference-matpower/script.m
+++ reference-matpower/script.m
@@ -1,4 +1,5 @@
-%  matlab /minimize /nosplash /nodesktop /r "filename='RTS-GMLC.m';script"
+% matlab -nodisplay -nosplash -nodesktop -r "filename='RTS_GMLC.m';script"
 
 mpc = loadcase(filename); % loadcase in matpower
 runpf(mpc, mpoption(), strrep(filename, '.m', '.log')); % runpf in matpower
+exit()
