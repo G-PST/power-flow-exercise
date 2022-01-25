35a134c257c0ae8a30bdca3f39582d4c7a097d23
Clayton Barrows <cbarrows@el2.ib0.cm.hpc.nrel.gov>
2022-01-24 20:57:23 -0700

    updating mat and log file

--- reference-matpower/script.m
+++ reference-matpower/script.m
@@ -3,7 +3,9 @@
 mpc = loadcase(filename); % loadcase in matpower
 opt = mpoption()
 opt.out.suppress_detail = 0
-log_file =  strrep(filename, '.m', '.log')
+log_file = strrep(filename, '.m', '.log')
+mat_file = strrep(filename, '.m', '.mat')
+savecase(mat_file, mpc)
 fclose(fopen(log_file, 'w'));
 res = runpf(mpc, opt, log_file); % runpf in matpower
 exit()
