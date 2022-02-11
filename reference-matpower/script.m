166e3a199cdec926a287ea2e2c1a96a7ac3e5ff4
Clayton Barrows <cbarrows@el3.ib0.cm.hpc.nrel.gov>
2022-02-11 10:32:49 -0700

    adding m and mat results

--- reference-matpower/script.m
+++ reference-matpower/script.m
@@ -1,16 +1,16 @@
 % matlab -nodisplay -nosplash -nodesktop -r "filename='RTS_GMLC/RTS_GMLC.m';script"
 
 mpc = loadcase(filename); % loadcase in matpower
-opt = mpoption()
-opt.out.suppress_detail = 0
+opt = mpoption();
+opt.out.suppress_detail = 0;
 [fPath, fName, fExt] = fileparts(filename);
-log_file = fullfile(fPath, fName, '.log')
-mat_file = fullfile(fPath, fName, '.mat')
-savecase(mat_file, mpc)
+log_file = fullfile(fPath, sprintf('%s.log', fName));
+mat_file = fullfile(fPath, sprintf('%s.mat', fName));
+savecase(mat_file, mpc);
 fclose(fopen(log_file, 'w'));
 res = runpf(mpc, opt, log_file); % runpf in matpower
-result_mat = fullfile(fPath, 'results', fName, '.mat')
-result_m = fullfile(fPath, 'results', fName, '.m')
-savecase(result_mat, mpc)
-savecase(result_m, mpc)
+result_mat = fullfile(fPath, 'results', sprintf('%s.mat', fName));
+result_m = fullfile(fPath, 'results', sprintf('%s.m', fName));
+savecase(result_mat, mpc);
+savecase(result_m, mpc);
 exit()
