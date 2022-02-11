b1d63e9081d75d32ccb5b3312bc620c76cdf2a7e
cbarrows <clayton.barrows@nrel.gov>
2022-02-11 10:20:48 -0700

    write result matpower files

--- reference-matpower/script.m
+++ reference-matpower/script.m
@@ -3,9 +3,14 @@
 mpc = loadcase(filename); % loadcase in matpower
 opt = mpoption()
 opt.out.suppress_detail = 0
-log_file = strrep(filename, '.m', '.log')
-mat_file = strrep(filename, '.m', '.mat')
+[fPath, fName, fExt] = fileparts(filename);
+log_file = fullfile(fPath, fName, '.log')
+mat_file = fullfile(fPath, fName, '.mat')
 savecase(mat_file, mpc)
 fclose(fopen(log_file, 'w'));
 res = runpf(mpc, opt, log_file); % runpf in matpower
+result_mat = fullfile(fPath, 'results', fName, '.mat')
+result_m = fullfile(fPath, 'results', fName, '.m')
+savecase(result_mat, mpc)
+savecase(result_m, mpc)
 exit()
