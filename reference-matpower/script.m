47af6fe41132d3d1a1c86ebaf1c551e460b8f505
cbarrows <clayton.barrows@nrel.gov>
2022-02-15 08:43:43 -0700

    finishing matpower script

--- reference-matpower/script.m
+++ reference-matpower/script.m
@@ -9,11 +9,12 @@ mat_file = fullfile(fPath, sprintf('%s.mat', fName));
 savecase(mat_file, mpc);
 fclose(fopen(log_file, 'w'));
 res = runpf(mpc, opt, log_file); % runpf in matpower
+res.losses = get_losses(res)
 result_mat = fullfile(fPath, 'results', sprintf('%s.mat', fName));
 result_m = fullfile(fPath, 'results', sprintf('%s.m', fName));
 savecase(result_mat, res);
 savecase(result_m, res);
-csvwrite('resline.csv', res.branch(res.branch(:,9)==0,:))
-csvwrite('restrafo.csv', res.branch(res.branch(:,9)~=0,:))
-csvwrite('resbus.csv', res.bus)
+csvwrite(fullfile(fPath, 'results', 'resline.csv'), res.branch(res.branch(:,9)==0,:))
+csvwrite(fullfile(fPath, 'results','restrafo.csv'), res.branch(res.branch(:,9)~=0,:))
+csvwrite(fullfile(fPath, 'results','resbus.csv'), res.bus)
 exit()
