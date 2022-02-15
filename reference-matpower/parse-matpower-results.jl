505ddcd1145b5dc2d8abc58075d2d364bf431bdf
cbarrows <clayton.barrows@nrel.gov>
2022-02-14 22:27:23 -0700

    changing matpower parser to read mat file

--- reference-matpower/parse-matpower-results.jl
+++ reference-matpower/parse-matpower-results.jl
@@ -1,115 +1,59 @@
 using CSV
 using DataFrames
+using MAT
 
+#bus_n,v_mag,v_ang,p_gen,q_gen,p_load,q_load
 function parse_bus(filename)
-    data = readlines(filename)
-    data = map(line -> strip(line), data)
-    data2 = []
-    PARSE = false
-    for line in data
-        if startswith(line, "|     Bus Data")
-            PARSE = true
-        end
-        if startswith(line, "|     Branch Data")
-            PARSE = false
-        end
-        if PARSE
-            push!(data2, line)
-        end
-    end
-    data = data2
-    data = filter(line -> !(startswith(line, "Total: ") || startswith(line, "=") || startswith(line, "-") || startswith(line, "|") || line == ""), data)
-    headers1 = popfirst!(data)
-    headers2 = popfirst!(data)
-    bus_n_arr = Union{Int64}[]
-    v_mag_arr = Union{Float64, Missing}[]
-    v_ang_arr = Union{Float64, Missing}[]
-    p_gen_arr = Union{Float64, Missing}[]
-    q_gen_arr = Union{Float64, Missing}[]
-    p_load_arr = Union{Float64, Missing}[]
-    q_load_arr = Union{Float64, Missing}[]
-    # λ_p_arr = Union{Float64, Missing}[]
-    # λ_q_arr = Union{Float64, Missing}[]
-    for (i, line) in enumerate(data)
-        bus_n, v_mag, v_ang, p_gen, q_gen, p_load, q_load = map(x -> x === nothing ? missing : x, tryparse.(Float64, split(line)))
-        # bus_n, v_mag, v_ang, p_gen, q_gen, p_load, q_load, λ_p, λ_q = map(x -> x === nothing ? missing : x, tryparse.(Float64, split(line)))
-        push!(bus_n_arr, Int(bus_n))
-        push!(v_mag_arr, v_mag)
-        push!(v_ang_arr, v_ang === missing ? 0 : v_ang)
-        push!(p_gen_arr, p_gen)
-        push!(q_gen_arr, q_gen)
-        push!(p_load_arr, p_load)
-        push!(q_load_arr, q_load)
-        # push!(λ_p_arr, λ_p)
-        # push!(λ_q_arr, λ_q)
-    end
-    DataFrame([
-        bus_n_arr,
-        v_mag_arr,
-        v_ang_arr,
-        p_gen_arr,
-        q_gen_arr,
-        p_load_arr,
-        q_load_arr,
-        # λ_p_arr,
-        # λ_q_arr,
-    ], [:bus_n, :v_mag, :v_ang, :p_gen, :q_gen, :p_load , :q_load])
+    results = matread(filename)
+    bus = results["mpc"]["bus"]
+    gen = results["mpc"]["gen"]
+
+    bus_df = DataFrame(bus[:, [1, 8, 9, 3, 4]], [:bus_n, :v_mag, :v_ang, :p_load, :q_load])
+    gen_df = combine(
+        groupby(DataFrame(gen[:, [1, 2, 3]], [:bus_n, :p_gen, :q_gen]), :bus_n),
+        [:p_gen, :q_gen] .=> sum,
+        renamecols = false,
+    )
+    leftjoin!(bus_df, gen_df, on = :bus_n)
+    return bus_df
 end
 
+#branch_n,from_bus_index,to_bus_index,from_bus_inj_p,from_bus_inj_q,to_bus_inj_p,to_bus_inj_q,loss_p,loss_q
 function parse_branch(filename)
-    data = readlines(filename)
-    data = map(line -> strip(line), data)
-    data2 = []
-    PARSE = false
-    for line in data
-        if startswith(line, "|     Branch Data")
-            PARSE = true
-        end
-        if PARSE
-            push!(data2, line)
-        end
+    results = matread(filename)
+    branch = results["mpc"]["branch"]
+    bus = results["mpc"]["bus"]
+
+    branch_df = DataFrame(
+        branch[:, [1, 2, 14, 15, 16, 17]],
+        [
+            :from_bus_index,
+            :to_bus_index,
+            :from_bus_inj_p,
+            :from_bus_inj_q,
+            :to_bus_inj_p,
+            :to_bus_inj_q,
+        ],
+    )
+    branch_df.branch_n = 1:nrow(branch_df)
+    loss = []
+    #loss = baseMVA * abs(V(e2i(branch(:, F_BUS))) ./ tap - V(e2i(branch(:, T_BUS)))) .^ 2 ./ (branch(:, BR_R) - 1j * branch(:, BR_X));
+    for b in eachrow(branch)
+        tap = b[9] == 0.0 ? 1.0 : b[9]
+        from_bus = findfirst(bus[:, 1] .== b[1])
+        to_bus = findfirst(bus[:, 1] .== b[2])
+        from_v = complex(bus[from_bus, 8], deg2rad(bus[from_bus, 9])) / tap
+        to_v = complex(bus[to_bus, 8], deg2rad(bus[to_bus, 9]))
+        push!(loss, results["baseMVA"] * abs(from_v - to_v)^2 / complex(b[3], -b[4]))
     end
-    data = data2
-    data = filter(line -> !(startswith(line, "Total: ") || startswith(line, "=") || startswith(line, "-") || startswith(line, "|")), data)
-    headers1 = popfirst!(data)
-    headers2 = popfirst!(data)
-    branch_n_arr = Union{Int64}[]
-    from_bus_index_arr = Union{Float64, Missing}[]
-    to_bus_index_arr = Union{Float64, Missing}[]
-    from_bus_inj_q_arr = Union{Float64, Missing}[]
-    from_bus_inj_p_arr = Union{Float64, Missing}[]
-    from_bus_inj_q_arr = Union{Float64, Missing}[]
-    to_bus_inj_p_arr = Union{Float64, Missing}[]
-    to_bus_inj_q_arr = Union{Float64, Missing}[]
-    loss_p_arr = Union{Float64, Missing}[]
-    loss_q_arr = Union{Float64, Missing}[]
-    for (i, line) in enumerate(data)
-        branch_n, from_bus_index, to_bus_index, from_bus_inj_p, from_bus_inj_q, to_bus_inj_p, to_bus_inj_q, loss_p, loss_q, = map(x -> x === nothing ? missing : x, tryparse.(Float64, split(line)))
-        push!(branch_n_arr, Int(branch_n))
-        push!(from_bus_index_arr, from_bus_index)
-        push!(to_bus_index_arr, to_bus_index)
-        push!(from_bus_inj_p_arr, from_bus_inj_p)
-        push!(from_bus_inj_q_arr, from_bus_inj_q)
-        push!(to_bus_inj_p_arr, to_bus_inj_p)
-        push!(to_bus_inj_q_arr, to_bus_inj_q)
-        push!(loss_p_arr, loss_p)
-        push!(loss_q_arr, loss_q)
-    end
-    DataFrame([
-        branch_n_arr,
-        from_bus_index_arr,
-        to_bus_index_arr,
-        from_bus_inj_p_arr,
-        from_bus_inj_q_arr,
-        to_bus_inj_p_arr,
-        to_bus_inj_q_arr,
-        loss_p_arr,
-        loss_q_arr,
-    ], [:branch_n, :from_bus_index, :to_bus_index, :from_bus_inj_p, :from_bus_inj_q, :to_bus_inj_p, :to_bus_inj_q, :loss_p , :loss_q])
+    branch_df.loss_p = real.(loss)
+    branch_df.loss_q = imag.(loss)
+
+    return branch_df
 end
 
 function main()
-    length(ARGS) == 0 && error("Must provide .log file to parse.")
+    length(ARGS) == 0 && error("Must provide .mat file to parse.")
     res_dir = mkpath(joinpath(dirname(ARGS[1]), "results"))
     CSV.write(joinpath(res_dir, "bus.csv"), parse_bus(ARGS[1]))
     CSV.write(joinpath(res_dir, "flow.csv"), parse_branch(ARGS[1]))
