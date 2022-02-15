05ddfb0b8913e521282b9f2a9ba9d6d8a610f246
Clayton Barrows <clayton.barrows@nrel.gov>
2022-02-15 08:32:35 -0700

    partial update of matpower script

--- reference-matpower/script.m
+++ reference-matpower/script.m
@@ -13,4 +13,7 @@ result_mat = fullfile(fPath, 'results', sprintf('%s.mat', fName));
 result_m = fullfile(fPath, 'results', sprintf('%s.m', fName));
 savecase(result_mat, res);
 savecase(result_m, res);
+csvwrite('resline.csv', res.branch(res.branch(:,9)==0,:))
+csvwrite('restrafo.csv', res.branch(res.branch(:,9)~=0,:))
+csvwrite('resbus.csv', res.bus)
 exit()
