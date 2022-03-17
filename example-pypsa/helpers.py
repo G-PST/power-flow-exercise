704954e44daebd1e4e00193e7db3fa32d9053c06
Max Parzen <max.parzen@ed.ac.uk>
2022-03-16 19:49:46 -0500

    update planning study

--- example-pypsa/helpers.py
+++ example-pypsa/helpers.py
@@ -13,10 +13,10 @@ import yaml
 from vresutils.costdata import annuity
 
 # Optional. Take local PyPSA-Dev installation to make adjustments
-# pypsa_path = os.getcwd()+"/PyPSA"  # require to have `PyPSA` cloned in ~/power-flow-exercise/example_pandapower/<PyPSA>`
-# import sys
-# sys.path.insert(0, f"{pypsa_path}")
-# import pypsa
+pypsa_path = os.getcwd()+"/PyPSA"  # require to have `PyPSA` cloned in ~/power-flow-exercise/example_pandapower/<PyPSA>`
+import sys
+sys.path.insert(0, f"{pypsa_path}")
+import pypsa
 
 
 def _sets_path_to_root(root_directory_name):
@@ -65,14 +65,73 @@ logging.basicConfig(filename=os.path.join("example-pypsa", "log.out"),
 
 logger = logging.getLogger(__name__)
 
+def check_net_ensure_unique_names(net):
+    for elm in net.keys():
+        if elm.startswith("_") or not isinstance(net[elm], pd.DataFrame) or len(net[elm]) == 0:
+            continue
+        # let's keep the bus names from mpc
+        if elm in ['bus', 'gen', 'sgen']:
+            continue
+        net[elm]['name'] = [f"{elm}_{i}" for i in net[elm].index.values]
+        # checking because in_service not supported by pypsa
+        if 'in_service' in net[elm].columns:
+            assert np.alltrue(net[elm]['in_service']), f"not in_service elements found in {elm}"
+        # checking because switches are not supported by pypsa
+        if elm == 'switch':
+            assert np.alltrue(net[elm]['closed']), "open switches found"
+        if elm == "trafo3w" and len(net[elm]) > 0:
+            logger.warning("found trafo3w in net (not supported by pypsa converter!!!)")
+        if elm == "shunt" and len(net[elm]) > 0:
+            logger.warning("found shunt in net (not supported by pypsa converter!!!)")
+
+
+def load_rts_grid():
+    # Attention: PyPSA takes name as index!!!!! Must be unique!
+    net = pp.converter.from_mpc(os.path.join("reference-matpower", "RTS_GMLC", "RTS_GMLC.mat"))
+    # make bus name match MATPOWER bus name
+    pp.toolbox.reindex_buses(net, net.bus.name + 1)
+    assign_additional_data(net)
+
+    # switching all sgens on to preserve information
+    # net.sgen.drop(net.sgen[~net.sgen.in_service].index, inplace=True)
+    net.sgen.in_service = True
+    check_net_ensure_unique_names(net)
+    pp.runpp(net)
+    return net
+
+
+def convert_to_pypsa(net):
+    network = pypsa.Network()
+    network.import_from_pandapower_net(net, True)
+
+    # now let's check some basic infos
+    assert len(network.buses) == len(net.bus)
+    assert len(network.generators) == (len(net.gen) + len(net.sgen) + len(net.ext_grid))
+    assert len(network.loads) == len(net.load)
+    assert len(network.transformers) == len(net.trafo)
+    # todo: shunt impedances are not supported
+    # todo: trafo tap positions are not supported
+    # todo: trafo3w are not supported
+    return network
+
+
+def assign_additional_data(net):
+    net.bus.name = net._options["bus_name"]
+    gen_lookup = net._options["gen_lookup"]
+    gen_name = net._options["gen_name"]
+    gen_lookup_gen = gen_lookup.query("element_type=='gen'")
+    gen_lookup_sgen = gen_lookup.query("element_type=='sgen'")
+    net.gen.loc[gen_lookup_gen.element.values, ['name', 'type', 'fuel']] = gen_name[gen_lookup_gen.index.values][:, [0, 1, 2]]
+    net.sgen.loc[gen_lookup_sgen.element.values, ['name', 'type', 'fuel']] = gen_name[gen_lookup_sgen.index.values][:, [0, 1, 2]]
+
 
 # # Comparing Pandapower to solved Matpower network
 def compare_to_matpower(net):
     """
     Compares pandapower network to matpower network.
     """
-    bus_results = pd.read_csv(os.path.join("reference-matpower", "results", "bus.csv"), index_col=0)
-    branch_results = pd.read_csv(os.path.join("reference-matpower", "results", "flow.csv"), index_col=0)
+    bus_results = pd.read_csv(os.path.join("reference-matpower", "RTS_GMLC","results", "bus.csv"), index_col=0)
+    branch_results = pd.read_csv(os.path.join("reference-matpower", "RTS_GMLC", "results", "flow.csv"), index_col=0)
 
     # compare bus results
     # merged_results = pd.merge(bus_results, net.res_bus, how='inner', left_index=True, right_on=net.bus.id)
